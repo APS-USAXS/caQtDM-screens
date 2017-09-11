bc972acddaa64e2382d7890a0ebc6d5c5005cc24
prjemian <prjemian@gmail.com>
2017-09-11 13:14:04 -0500

    developer use

---
+++ set_screen_paths.sh
@@ -0,0 +1,13 @@
+#this file used by local developer who 
+# skips the start_caQtDM_usaxs.sh script
+
+# others IGNORE it
+
+export CAQTDM_DISPLAY_PATH=`pwd`
+export CAQTDM_DISPLAY_PATH+=:`pwd`/usaxs
+export CAQTDM_DISPLAY_PATH+=:`pwd`/saxs
+export CAQTDM_DISPLAY_PATH+=:`pwd`/user
+export CAQTDM_DISPLAY_PATH+=:`pwd`/hardware
+export CAQTDM_DISPLAY_PATH+=:`pwd`/general
+export CAQTDM_DISPLAY_PATH+=:`pwd`/beamline
+export CAQTDM_DISPLAY_PATH+=:`pwd`/legacy

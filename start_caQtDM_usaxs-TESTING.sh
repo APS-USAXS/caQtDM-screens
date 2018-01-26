215c48155d2acaacaf0c1cf07e4abae87075b729
Service Account for Jan Ilavsky <usaxs@usaxsserver.xray.aps.anl.gov>
2018-01-26 11:15:18 -0600

    Added bleps and 9idBLEPS dirs to the caQtDM display path

--- start_caQtDM_usaxs-TESTING.sh
+++ start_caQtDM_usaxs-TESTING.sh
@@ -86,6 +86,7 @@ addScreenPath      BEAMLINE_UI_DIR       general
 addScreenPath      BEAMLINE_UI_DIR       beamline
 addScreenPath      BEAMLINE_UI_DIR       user
 addScreenPath      BEAMLINE_UI_DIR       ../9ida
+addScreenPath      BEAMLINE_UI_DIR       ../bleps
 addScreenPath      BEAMLINE_UI_DIR       legacy
 addScreenPath      DSERV_EPICS_DIR       synApps_5_7/ioc/9idcLAX/9idcLAXApp/op/adl
 addScreenPath      DSERV_EPICS_DIR       synApps_5_8/ioc/9idcRIO/9idcRIOApp/op/adl
@@ -94,6 +95,8 @@ addScreenPath      DSERV_EPICS_DIR       synApps_5_7/ioc/9id/9idApp/op/adl
 addScreenPath      DSERV_EPICS_DIR       synApps_5_7/ioc/9id/9idaApp/op/adl
 addScreenPath      DSERV_EPICS_DIR       synApps_5_7/ioc/9id/9idbApp/op/adl
 addScreenPath      DSERV_EPICS_DIR       synApps_5_7/ioc/9id/9idcApp/op/adl
+addScreenPath      DSERV_EPICS_DIR       synApps_5_7/ioc/9id/9idcApp/op/adl
+addScreenPath      DSERV_EPICS_DIR       synApps_5_7/ioc/9idBLEPS/9idBLEPSApp/op/adl
 addScreenPath      ALIVE                 aliveApp/op/ui
 addScreenPath      ALIVE                 aliveApp/op/ui/autoconvert
 # AREA_DETECTOR needs to be defined because xxx doesn't include it
